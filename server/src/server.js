b51ad52d538f9290859d0fd86b5771647e5bfd0a
pelmeh <nikki2410@yandex.ru>
2017-06-18 23:05:06 +0300

    github pages

--- server/src/server.js
+++ server/src/server.js
@@ -32,8 +32,6 @@ function render (html, preloadedState) {
       <body>
         <div id="root">${html}</div>
         <script>
-          // WARNING: See the following for security issues around embedding JSON in HTML:
-          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
         <script src="/bundle.js"></script>
